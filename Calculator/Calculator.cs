0b3d350469a729472b9b86134d69b5ef6184015b
Anjou <krzysztof.jan.kroczak@gmail.com>
2016-03-02 23:01:32 +0100

    poprawki1 -> nie każdy projekt musi być konsolowy; twoje testy mogą być zwykłą biblioteką .dll Był tutaj również plik, który był niepotrzebny... ONPOperation.cs

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,104 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text; //dla StringBuilder
 
 namespace ONP
 {
-    public class Calculator
+	public class Calculator
     {
-        public double Calculate(string operation)
+        public double Calculate(string expression)
         {
-			if (operation == "") throw new EmptyStringException();
-            var operationElements = OperationToElements(operation);
-			var operationONP = OperationElementsToONP(operationElements);
+			//a co jak null? proponuję:
+			if (string.IsNullOrEmpty(expression)) throw new EmptyStringException();
+            var operationElements = GetOperationElements(expression);
+			var operationInOnpOrder = SortElementsInOnpOrder(operationElements);
 
-			Stack<double> operationNumbers = new Stack<double>();
-			
-
-			foreach (var operationElement in operationONP)
+			var operationNumbers = new Stack<double>();
+			foreach (var operationElement in operationInOnpOrder)
 			{
-				if (operationElement.Type == OperationElementType.NUMBER)
-					operationNumbers.Push(Double.Parse(operationElement.Value));
+				if (operationElement.Type == OperationElementType.Number)
+				{
+					operationNumbers.Push(double.Parse(operationElement.Value));
+				}
 				else
 				{
 					var secondNumber = operationNumbers.Pop();
 					var firstNumber = operationNumbers.Pop(); //Kolejność ściągania ze stosu jest tutaj kluczowa
-					operationNumbers.Push(CommitOperation(operationElement.Value, firstNumber, secondNumber));
+					operationNumbers.Push(CalculateOperation(operationElement.Value, firstNumber, secondNumber));
 				}
 			}
 			return operationNumbers.Pop();
         }
 
-		private double CommitOperation(string operation, double firstNumber, double secondNumber)
-		{
-			switch (operation)
-			{
-				case "+": return firstNumber + secondNumber;
-				case "-": return firstNumber - secondNumber;
-				case "*": return firstNumber * secondNumber;
-				case "/": return firstNumber / secondNumber;
-				default: throw new IncorrectOperationStringException(String.Format("Not supported operator used: {0}", operation));
-			}
-		}
-
-		public Queue<OperationElement> OperationToElements(string operation)
+		//nazwy metod powinny zawierać w sobie czasownik, dzięki temu od razu można odgadnąć, co robi dana metoda
+		//czy potrzeba tutaj kolejki? Tutaj następuje zwykła transformacja
+		public List<OperationElement> GetOperationElements(string operation)
         {
-            var operationElements = new Queue<OperationElement>();
-            string bufforForLongerNumbers = "";
+            var operationElements = new List<OperationElement>();
+			//buffor w postaci stringa to zły pomysł
+			//stringi w C# są niemutuwalne, czyli dodając znak do stringa nie zmieniasz wcześniejszej instancji
+			//lecz dostajesz nową instancję!!
+			//istnieje dedykowany mechanizm StringBuilder dla takich rzeczy
+			var builder = new StringBuilder();
 
             foreach (char character in operation)
             {
-                if (Char.IsDigit(character)) bufforForLongerNumbers += character;
-                else if (IsCharacterAnOperator(character))
-                {
-                    if (bufforForLongerNumbers != "")
-                    {
-                        operationElements.Enqueue(new OperationElement(OperationElementType.NUMBER, bufforForLongerNumbers));
-                        bufforForLongerNumbers = "";
-                    }
-                    operationElements.Enqueue(new OperationElement(OperationElementType.OPERATOR,character.ToString()));
-                }
-                else throw new IncorrectOperationStringException("Incorrect operation string: " + operation);
+				//u mnie w pracy jest konwencja, że jeżeli gdziekolwiek w warunku
+				//zapiszesz w {} to wszystkie inne też należy pisać
+				//we wcześniejszej pracy chcieli zawsze pisać w {}
+				//wynika to z tego, że łatwo się pomylić
+				//patrz przykład w nowym pliku ExampleIfProblem.txt
+				if (char.IsDigit(character))
+				{
+					builder.Append(character);
+				}
+				else if (IsOperator(character))
+				{
+					if (builder.Length != 0)
+					{
+						operationElements.Add(new OperationElement(OperationElementType.Number, builder.ToString()));
+						builder.Clear();
+					}
+					operationElements.Add(new OperationElement(OperationElementType.Operator, character.ToString()));
+				}
+				else
+				{
+					throw new IncorrectOperationStringException(operation);
+				}
             }
-            if (bufforForLongerNumbers != "") operationElements.Enqueue(new OperationElement(OperationElementType.NUMBER, bufforForLongerNumbers));
+            if (builder.Length != 0)
+				operationElements.Add(new OperationElement(OperationElementType.Number, builder.ToString()));
             return operationElements;
         }
 
-        private bool IsCharacterAnOperator(char character)
+		//podobnie jak wcześniej nie ma czasownika i nie do końca wiadomo co robi metoda
+		//ta metoda również nie musiałaby zwracać kolejki, lecz mogłaby listę
+		public Queue<OperationElement> SortElementsInOnpOrder(IList<OperationElement> operationElements)
         {
-            return (character == '+'
-                || character == '-'
-                || character == '*'
-                || character == '/'
-                || character == '('
-                || character == ')') ;
-        }
-
-        public Queue<OperationElement> OperationElementsToONP(Queue<OperationElement> operationElements)
-        {
-            Queue<OperationElement> result = new Queue<OperationElement>();
-            Stack<OperationElement> operators = new Stack<OperationElement>();
+            var elementsInOnpOrder = new Queue<OperationElement>();
+            var operatorsStack = new Stack<OperationElement>();
             foreach (var element in operationElements)
             {
-                if (element.Type == OperationElementType.NUMBER)
-                    result.Enqueue(element);
-                else if (element.Value == "(") operators.Push(element);
-                else if (element.Value == ")")
-                {
-                    while (operators.Peek().Value != "(") result.Enqueue(operators.Pop());
-                    operators.Pop();
-                }
-                else if (element.Type == OperationElementType.OPERATOR)
-                {
-                    while (operators.Count != 0 && DesignateOperatorPriority(element) <= DesignateOperatorPriority(operators.Peek()))
-                        result.Enqueue(operators.Pop());
-                    operators.Push(element);
-                }
+				if (element.Type == OperationElementType.Number)
+				{
+					elementsInOnpOrder.Enqueue(element);
+				}
+				else //wiem, że mamy zagnieżdżenie jednakże gdybyśmy to wyciągnęli to możemy to jeszcze ładniej zapisać
+				{
+					if (element.Value == "(")
+					{
+						operatorsStack.Push(element);
+					}
+					else if (element.Value == ")")
+					{
+						//? ta pętla się zapętla?!
+						while (operatorsStack.Peek().Value != "(")
+							elementsInOnpOrder.Enqueue(operatorsStack.Pop());
+						operatorsStack.Pop();
+					}
+					else
+					{
+						while (operatorsStack.Count != 0 && GetOperatorPriority(element) <= GetOperatorPriority(operatorsStack.Peek()))
+							elementsInOnpOrder.Enqueue(operatorsStack.Pop());
+						operatorsStack.Push(element);
+					}
+				}
             }
-            while (operators.Count != 0) result.Enqueue(operators.Pop());
-            return result;
+            while (operatorsStack.Count != 0)
+				elementsInOnpOrder.Enqueue(operatorsStack.Pop());
+            return elementsInOnpOrder;
         }
 
-        private int DesignateOperatorPriority(OperationElement element)
+		//Jedna z konwencji proponuje mieć publiczne rzeczy na górze potem prywatne
+		//są różne konwencje
+		//ważne by zespół miał wspólną, dzieki czemu łatwiej się czyta kod, bo wiadomo, gdzie spodziewać się danych rzeczy
+
+		//Preferuję proste czasowniki :P
+        private int GetOperatorPriority(OperationElement element)
         {
             if (element.Value == "(")
                 return 0;
@@ -109,5 +124,31 @@ namespace ONP
             else return 2;
         }
 
-    }
+		//Commit - nie kojarzy mi się z tą operacją
+		//http://www.oxforddictionaries.com/definition/english/commit przykłady użycia też nie odnoszą się raczej do tego
+		//Tutaj chcesz wyliczyć daną operację
+		private double CalculateOperation(string operation, double firstNumber, double secondNumber)
+		{
+			switch (operation)
+			{
+				case "+": return firstNumber + secondNumber;
+				case "-": return firstNumber - secondNumber;
+				case "*": return firstNumber * secondNumber;
+				case "/": return firstNumber / secondNumber;
+				default: throw new IncorrectOperationStringException(operation);
+			}
+		}
+
+		//proponuję krótszą nazwę
+		private bool IsOperator(char character)
+		{
+			return (character == '+'
+				|| character == '-'
+				|| character == '*'
+				|| character == '/'
+				|| character == '('
+				|| character == ')');
+		}
+
+	}
 }
--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -1,7 +1,8 @@
 ﻿using NUnit.Framework;
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace ONP.Tests
 {
@@ -56,125 +57,190 @@ namespace ONP.Tests
             Assert.Throws<EmptyStringException>(() => calc.Calculate(""));
         }
 		[Test]
-		public void Calculate_ValidOperationString_ReturnsAnswer1()
+		public void Calculate_EmptyString_ThrowsEmptyStringException()
+		{
+			//Assign
+			var calc = new Calculator();
+
+			//Act
+			TestDelegate t = () => calc.Calculate("");
+
+			//Assert
+			Assert.Throws<EmptyStringException>(t);
+		}
+
+		[Test]
+		public void Calculate_ValidOperationString_Returns1()
 		{
 			var calc = new Calculator();
-			var result = calc.Calculate("3+2");
 			var expectation = 5;
-			Assert.AreEqual(expectation, result);
+
+			var sumResult = calc.Calculate("3+2");
+
+			Assert.AreEqual(expectation, sumResult);
 		}
+
 		[Test]
-		public void Calculate_ValidOperationString_ReturnsAnswer2()
+		public void Calculate_WhenAllOperatorUsedInExpression_Returns23_5()
 		{
 			var calc = new Calculator();
-			var result = calc.Calculate("((2+7)/3+(14-3)*4)/2");
+
+			var resultOfAllOperatorUsedInExpression = calc.Calculate("((2+7)/3+(14-3)*4)/2");
+
 			var expectation = 23.5;
-			Assert.AreEqual(expectation, result);
+			Assert.AreEqual(expectation, resultOfAllOperatorUsedInExpression);
+		}
+
+		[Test]
+		public void GetOperationElements_InvalidOperationString_ThrowsIncorrectOperationStringException()
+		{
+			var calc = new Calculator();
+
+			TestDelegate t = () => calc.GetOperationElements("2!2");
+
+			Assert.Throws<IncorrectOperationStringException>(t);
 		}
 		[Test]
-        public void OperationToElements_invalidOperationString_ThrowsIncorrectOperationStringException()
-        {
-            var calc = new Calculator();
-            Assert.Throws<IncorrectOperationStringException>(() => calc.OperationToElements("2!2"));
-        }
-        [Test]
-        public void OperationToElements_validOperationString_ElementsInQueue()
-        {
-            var calc = new Calculator();
-            var result = calc.OperationToElements("3+2");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER,"3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-        [Test]
-        public void OperationToElements_validOperationString_ElementsInQueue2()
-        {
-            var calc = new Calculator();
-            var result = calc.OperationToElements("(3+2)-2");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "("));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, ")"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "-"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-        [Test]
-        public void OperationElementsToONP_validOperationElements_ElementsInONPQueue()
-        {
-            var calc = new Calculator();
-            var operationElements = new Queue<OperationElement>();
-            operationElements = calc.OperationToElements("3+2");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            var result = calc.OperationElementsToONP(operationElements);
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-        [Test]
-        public void OperationElementsToONP_validOperationElements_ElementsInONPQueue2() 
-        {
-            var calc = new Calculator();
-            var operationElements = new Queue<OperationElement>();
-            operationElements = calc.OperationToElements("2+3*5");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "5"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "*"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            var result = calc.OperationElementsToONP(operationElements);
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-        [Test]
-        public void OperationElementsToONP_validOperationElements_ElementsInONPQueue3()
-        {
-            var calc = new Calculator();
-            var operationElements = new Queue<OperationElement>();
-            operationElements = calc.OperationToElements("(2+3)*5");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "5"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "*"));
-            var result = calc.OperationElementsToONP(operationElements);
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-        [Test]
-        public void OperationElementsToONP_validOperationElements_ElementsInONPQueue4()
-        {
-            var calc = new Calculator();
-            var operationElements = new Queue<OperationElement>();
-            operationElements = calc.OperationToElements("((2+7)/3+(14-3)*4)/2");
-            var expectation = new Queue<OperationElement>();
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "7"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "/"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "14"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "3"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "-"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "4"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "*"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "+"));
-            expectation.Enqueue(new OperationElement(OperationElementType.NUMBER, "2"));
-            expectation.Enqueue(new OperationElement(OperationElementType.OPERATOR, "/"));
-            var result = calc.OperationElementsToONP(operationElements);
-            Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
-        }
-    }
-    public static class TestValues
-    {
-        public static string ToTestValue(this OperationElement oe)
-        {
-            return $"type: {oe.Type} value: {oe.Value}";
-        }
-    }
+		public void GetOperationElements_ValidOperationString_ElementsInQueue()
+		{
+			var calc = new Calculator();
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Number,"3" },
+				{ OperationElementType.Operator, "+"},
+				{ OperationElementType.Number, "2"}
+			}.Cast<OperationElement>();
+
+			var result = calc.GetOperationElements("3+2");
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+
+		[Test]
+		public void OperationToElements_validOperationString_ElementsInQueue2()
+		{
+			var calc = new Calculator();
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Operator, "(" },
+				{ OperationElementType.Number, "3" },
+				{ OperationElementType.Operator, "+" },
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Operator, ")" },
+				{ OperationElementType.Operator, "-" },
+				{ OperationElementType.Number, "2" }
+			}.Cast<OperationElement>();
+
+			var result = calc.GetOperationElements("(3+2)-2");
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+
+		[Test]
+		public void SortElementsInOnpOrder_validOperationElements_ElementsInONPQueue()
+		{
+			var calc = new Calculator();
+			var operationElements = calc.GetOperationElements("3+2");
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Number, "3" },
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Operator, "+" }
+			}.Cast<OperationElement>();
+
+			var result = calc.SortElementsInOnpOrder(operationElements);
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+		[Test]
+		public void SortElementsInOnpOrder_validOperationElements_ElementsInONPQueue2()
+		{
+			var calc = new Calculator();
+			var operationElements = calc.GetOperationElements("2+3*5");
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Number, "3" },
+				{ OperationElementType.Number, "5" },
+				{ OperationElementType.Operator, "*" },
+				{ OperationElementType.Operator, "+" }
+			}.Cast<OperationElement>();
+
+			var result = calc.SortElementsInOnpOrder(operationElements);
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+		[Test]
+		public void SortElementsInOnpOrder_validOperationElements_ElementsInONPQueue3()
+		{
+			var calc = new Calculator();
+			var operationElements = calc.GetOperationElements("(2+3)*5");
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Number, "3"},
+				{ OperationElementType.Operator, "+"},
+				{ OperationElementType.Number, "5"},
+				{ OperationElementType.Operator, "*"}
+			}.Cast<OperationElement>();
+
+			var result = calc.SortElementsInOnpOrder(operationElements);
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+		[Test]
+		public void SortElementsInOnpOrder_validOperationElements_ElementsInONPQueue4()
+		{
+			var calc = new Calculator();
+			var operationElements = calc.GetOperationElements("((2+7)/3+(14-3)*4)/2");
+			var expectation = new OperationElements
+			{
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Number, "7" },
+				{ OperationElementType.Operator, "+" },
+				{ OperationElementType.Number, "3" },
+				{ OperationElementType.Operator, "/" },
+				{ OperationElementType.Number, "14" },
+				{ OperationElementType.Number, "3" },
+				{ OperationElementType.Operator, "-" },
+				{ OperationElementType.Number, "4" },
+				{ OperationElementType.Operator, "*" },
+				{ OperationElementType.Operator, "+" },
+				{ OperationElementType.Number, "2" },
+				{ OperationElementType.Operator, "/" }
+			}.Cast<OperationElement>();
+
+			var result = calc.SortElementsInOnpOrder(operationElements);
+
+			Assert.AreEqual(expectation.Select(r => r.ToTestValue()), result.Select(r => r.ToTestValue()));
+		}
+
+		private class OperationElements : IEnumerable
+		{
+			public void Add(OperationElementType type, string value)
+			{
+				operations.Add(new OperationElement(type, value));
+			}
+
+			public IEnumerator GetEnumerator()
+			{
+				return operations.GetEnumerator();
+			}
+
+			internal double Select(Func<object, string> p)
+			{
+				throw new NotImplementedException();
+			}
+
+			readonly List<OperationElement> operations = new List<OperationElement>();
+		}
+	}
+
+	public static class TestValues
+	{
+		public static string ToTestValue(this OperationElement oe)
+		{
+			return $"type: {oe.Type} value: {oe.Value}";
+		}
+	}
 }
